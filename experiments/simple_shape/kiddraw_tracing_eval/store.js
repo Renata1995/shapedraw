34b22d8b52433f5a6f22fa2143056b6278545485
Judy Fan <judithfan@gmail.com>
2018-12-04 16:17:07 -0800

    tracing eval is the only collection we need to check

--- experiments/simple_shape/kiddraw_tracing_eval/store.js
+++ experiments/simple_shape/kiddraw_tracing_eval/store.js
@@ -85,9 +85,7 @@ function serve() {
       const projection = request.body.projection;
 
       // hardcoded for now (TODO: get list of collections in db)
-      var collectionList = ['sketchpad_basic','sketchpad_repeated',
-          'chatbox_basic', 'graphical_conventions', 'graphical_conventions_recog', //'chairs_chatbox',
-          'artificialLanguage','tracing_eval']; 
+      var collectionList = ['tracing_eval']; 
 
       function checkCollectionForHits(collectionName, query, projection, callback) {
         const collection = database.collection(collectionName);        
