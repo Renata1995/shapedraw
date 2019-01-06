6c22d878b957171e926392d6a4ecb9fabd8bf13d
Judy Fan <judithfan@gmail.com>
2019-01-06 10:15:29 -0800

    change default port

--- experiments/simple_shape/kiddraw_tracing_eval/app.js
+++ experiments/simple_shape/kiddraw_tracing_eval/app.js
@@ -22,7 +22,7 @@ if(argv.gameport) {
     gameport = argv.gameport;
     console.log('using port ' + gameport);
 } else {
-    gameport = 8888;
+    gameport = 8887;
     console.log('no gameport specified: using 8888\nUse the --gameport flag to change');
 }
 
